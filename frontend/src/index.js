bec1a765afd30ce5ab6d1fcd0478c2bcf915a89a
Ajzboss <48899953+Ajzboss@users.noreply.github.com>
2025-02-16 17:06:33 -0800

    Revert "Created Basic Api between gamemaster and frontend"
    This reverts commit 63ce1ab269e83550820b97ba0003c07b56e452ac.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Leaderboard from "./components/Leaderboard";
 import Quiz from "./components/Quiz";
-import UserInputTest from "./components/UserInputTest";
-
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -17,7 +15,6 @@ root.render(
                 <Route path="/" element={<Login />} />
                 <Route path="/quiz" element={<Quiz />} />
                 <Route path="/leaderboard" element={<Leaderboard />} />
-                <Route path="/userinputtest" element={<UserInputTest/>} />
             </Routes>
         </Router>
     </React.StrictMode>
