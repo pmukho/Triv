687bc36cc2205161a89d3ff8531ca692cd739b20
Geoffrey  Xu <geoffreyxu@wifi-131-179-13-48.host.ucla.edu>
2025-02-19 12:46:06 -0800

    Test batching from cache

--- cache/main.py
+++ cache/main.py
@@ -11,7 +11,7 @@ DB_CONFIG = {
     "user": os.environ.get("POSTGRES_USER"),
     "password": os.environ.get("POSTGRES_PASSWORD"),
     "host": "postgres-db",
-    "port": "5432"
+    "port": "5433"
 }
 
 def get_db_connection():
