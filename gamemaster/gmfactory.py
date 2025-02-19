9a56283becc6435d2e3eb39f66e3e971696764be
Ajzboss <48899953+Ajzboss@users.noreply.github.com>
2025-02-19 00:40:53 -0800

    Seemed to have fixed the bug with docker compose

--- gamemaster/gmfactory.py
+++ gamemaster/gmfactory.py
@@ -4,7 +4,6 @@ import asyncio
 import uvicorn
 from gamemaster import GameMaster
 
-app = FastAPI()
 class Question(BaseModel):
     id: str
     text: str
@@ -29,7 +28,6 @@ class Data(BaseModel):
 class GmFactory:
     def __init__(self):
         self.game_masters = {}
-        self.app = app
         # Initialize the game_master_number to 0, could cause issues with too many game masters
         self.game_master_number = 0
     def get_or_create_game_master(self, client_id: int) -> int:
@@ -39,7 +37,3 @@ class GmFactory:
             print(f"Game Master with client_id: {client_id} created")
         return client_id
 
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
