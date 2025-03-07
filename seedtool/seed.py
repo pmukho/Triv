5c7e7e64dfe685a5418cf7865af1c8d97770a5b9
pmukho <pmukho2003@gmail.com>
2025-03-06 17:09:47 -0800

    Randomizing order of article in category

--- seedtool/seed.py
+++ seedtool/seed.py
@@ -5,6 +5,7 @@ import psycopg2
 from psycopg2.extras import execute_batch
 from typing import List, Dict
 import os
+import random
 
 DB_CONFIG = {
     "dbname": os.environ.get("POSTGRES_DB"),
@@ -57,6 +58,7 @@ def fetch_petscan_data(psid: str):
                                     'category': psid_to_category.get(psid, 'Unknown')
                                 })
             
+        random.shuffle(articles)
         return articles
             
     except requests.RequestException as e:
@@ -68,29 +70,6 @@ def fetch_petscan_data(psid: str):
     except Exception as e:
         print(f"Unexpected error: {str(e)}")
         return []
-
-# def connect_to_db() -> psycopg2.extensions.connection:
-#     try:
-#         return psycopg2.connect(**DB_CONFIG)
-#     except psycopg2.Error as e:
-#         print(f"Database connection error: {e}")
-
-# def insert_articles(conn: psycopg2.extensions.connection, articles: List[Dict]):
-#     with conn.cursor() as cur:
-#         query = """
-#             INSERT INTO wiki_articles (title, category)
-#             VALUES (%s, %s)
-#             ON CONFLICT (title) DO UPDATE 
-#             SET category = EXCLUDED.category
-#         """
-        
-#         article_data = [(
-#             article['title'],
-#             article['category']
-#         ) for article in articles]
-        
-#         execute_batch(cur, query, article_data)
-#     conn.commit()
 
 def main():
     output_dir = "./db/data"
