22c8a04a19db1a6ed5bca36c60a61634f2f4f9a7
pmukho <pmukho2003@gmail.com>
2025-03-09 18:33:47 -0700

    wrong routing path fix

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -194,7 +194,7 @@ const Quiz = () => {
     setCurrentQuestion((prev) => {
       const nextNumber = prev.questionNumber + 1;
       if (nextNumber > maxQuestions) {
-        navigate('/CategoryResults');
+        navigate('/results');
         return prev; // Returning prev since we don't need to update state further
       }
       return {
